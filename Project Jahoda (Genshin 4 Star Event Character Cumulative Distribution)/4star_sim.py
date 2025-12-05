bdc4d7513332c1fc5a3ab0d7632100f30340284c
MW <wong.max.c@gmail.com>
2025-12-04 20:19:05 -0500

    Cleaned up even more

--- Project Jahoda (Genshin 4 Star Event Character Cumulative Distribution)/4star_sim.py
+++ Project Jahoda (Genshin 4 Star Event Character Cumulative Distribution)/4star_sim.py
@@ -1,14 +1,3 @@
-### Sources:
-# https://www.hoyolab.com/article/31323555
-# https://genshin-impact.fandom.com/wiki/Wish
-
-# HOW TO USE
-# Run "pip install numpy"
-# Run "pip install matplotlib"
-# 
-# Run "py .\4star_sim.py"
-
-import numpy as np
 from numpy import random
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
@@ -17,25 +6,19 @@ from matplotlib.ticker import MultipleLocator
 N = 1000000 # How many pull periods
 RUN_CAP = 150 # How may pulls before giving up
 
-# Probabilities
 P_4STAR_BASE = 0.051 # Base 
 P_EVENT_4STAR = 0.5 # 50% of event char, 50% of regular 4 star or weapon
 P_GUARANTEE_4STAR = 0.994 # 99.4% 4 star, 0.6% 5 star
 P_SPECIFIC_EVENT_4STAR = 1/3 # After winnning specific 4 star char, 1/3
-
 GUARANTEE_4STAR = 10 # Guaranteed to win 4/5 star if no 10 in a row
-# 2x10 pulls guarantee event character
 
 # Determines if win or loss given a probability
 def draw(probability):
-    x = random.rand()
-    return x < probability # ex: 50% -> middle is 500. 0-499 lose, 500-999 win
+    return random.rand() < probability # ex: 50% -> middle is 500. 0-499 lose, 500-999 win
 
 # returns necessary data for if drawing an event char was 4 star or 5 star
 def draw_evenchar():
-    # Won specific event 4 star
-    if draw(P_GUARANTEE_4STAR) and draw(P_SPECIFIC_EVENT_4STAR): return [0, False, True]
-    return [0, False, False] # Otherwise, won someone else (not specific 4 star, or 5 star)
+    return [0, False, draw(P_GUARANTEE_4STAR) and draw(P_SPECIFIC_EVENT_4STAR)]
 
 # In charge of simulating each pull
 # Requires and updates number of pulls without win, and if an event character is guaranteed
@@ -43,40 +26,24 @@ def run_pull(pulls_without_win, lost_event_char):
     pulls_without_win += 1
     if pulls_without_win == GUARANTEE_4STAR:
         # Guarantee 4/5 star
-        if lost_event_char:
-            # guarantee event character (20 pulls)
-            return draw_evenchar()
-        else:
-            # Won character (normal 10 pull)
-            if draw(P_EVENT_4STAR):
-                # Won event 4 star character
-                return draw_evenchar()
-            else:
-                # Won regular 4 star character
-                return [0, True, False]
-    else:
-        # normal pull
-        if draw(P_4STAR_BASE):
-            # Win 4 star
+        if lost_event_char:  return draw_evenchar() # guarantee event character (20 pulls)
+        else: # Won character (normal 10 pull)
+            if draw(P_EVENT_4STAR): return draw_evenchar() # Won event 4 star character
+            else: return [0, True, False] # Won regular 4 star character
+    else: # normal pull
+        if draw(P_4STAR_BASE): # Win 4 star
             if lost_event_char or draw(P_EVENT_4STAR):
-                # Won event 4 star character
-                return draw_evenchar()
+                return draw_evenchar() # Won event 4 star character
             else:
-                # Won regular 4 star character
-                return [0, True, False]
-        else:
-            # No win
-            return [pulls_without_win, lost_event_char, False]
-
-
+                return [0, True, False] # Won regular 4 star character
+        else: return [pulls_without_win, lost_event_char, False] # No win
+            
 ### Main
 raw_data = [0] * RUN_CAP
-p_data = [0] * RUN_CAP
 y_data = [0] * RUN_CAP
 x_data = list(range(1, RUN_CAP+1))
-total = 0
-
 N_counter = 0 # how many pull periods
+
 print("Running")
 while (N_counter < N):
     # Reset
@@ -101,8 +68,7 @@ while (N_counter < N):
 y_data[0] = (raw_data[0]/N)
 for j in range(1, RUN_CAP):
     y_data[j] = y_data[j-1] + (raw_data[j]/N)
-    assert(y_data[j] > y_data[j-1])
-    
+
 # Can plot cumulative distribution with "data" here
 print (x_data)
 print (y_data)
---
+++ Project Jahoda (Genshin 4 Star Event Character Cumulative Distribution)/README.md
@@ -0,0 +1,18 @@
+# HOW TO USE
+
+### Install
+
+```bash
+pip install numpy
+pip install matplotlib
+```
+
+### Run
+
+```bash
+py .\4star_sim.py
+```
+
+# Sources:
+
+### https://genshin-impact.fandom.com/wiki/Wish
