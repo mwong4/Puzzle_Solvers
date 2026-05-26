8fc6828f6f891f177d9fa1071b5fb40f053e464d
MW <wong.max.c@gmail.com>
2026-05-26 10:55:28 -0400

    learning tkinter

--- Wordle/solver.py
+++ Wordle/solver.py
@@ -1,7 +1,8 @@
 import time
 import pandas as pd 
 import json
-
+import tkinter as tk
+from tkinter import ttk
 
 # Must be non empty
 # INITIAL_GUESSES = ["hates", "round", "climb"]
@@ -186,6 +187,63 @@ def testing_runner():
 
 
 if __name__ == '__main__':
-    words = pre_solver(False)
-    solver("testinggg", False, False, words, INITIAL_GUESSES) # Normal 
+    # GUI
+    root = tk.Tk()
+    root.title("Wordle Solver")
+
+    def add_to_list(event=None):
+        text = entry.get()
+        if text:
+            text_list.insert(tk.END, text)
+            entry.delete(0, tk.END)
+
+    root.columnconfigure(0, weight=1)
+    root.columnconfigure(1, weight=3)
+    root.rowconfigure(0, weight=1)
+
+    ## Frame
+    frame = ttk.Frame(root)
+    frame.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
+
+    frame.columnconfigure(0, weight=1)
+    frame.rowconfigure(1, weight=1)
+
+    entry = ttk.Entry(frame)
+    entry.grid(row=0, column=0, sticky="ew")
+
+    # entry.bind("<Return>", lambda event: add_to_list())
+    entry.bind("<Return>", add_to_list)
+
+    entry_btn = ttk.Button(frame, text="Add", command=add_to_list)
+    entry_btn.grid(row=0, column=1)
+
+    text_list = tk.Listbox(frame)
+    text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
+    ## Frame end
+
+    ## Frame 2
+    frame2 = tk.Frame(root)
+    frame2.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
+
+    frame2.columnconfigure(0, weight=1)
+    frame2.rowconfigure(1, weight=1)
+
+    entry = tk.Entry(frame2)
+    entry.grid(row=0, column=0, sticky="ew")
+
+    # entry.bind("<Return>", lambda event: add_to_list())
+    entry.bind("<Return>", add_to_list)
+
+    entry_btn = tk.Button(frame2, text="Add", command=add_to_list)
+    entry_btn.grid(row=0, column=1)
+
+    text_list = tk.Listbox(frame2)
+    text_list.grid(row=1, column=0, columnspan=2, sticky="nsew")
+    ## Frame end
+
+    root.mainloop()
+
+
+    # words = pre_solver(False)
+    # solver("testinggg", False, False, words, INITIAL_GUESSES) # Normal 
     # testing_runner() # Tester
